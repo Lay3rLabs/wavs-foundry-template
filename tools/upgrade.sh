60daa59ad8e942f075295279c67287e1933d1d2b
Ethan Frey <ethanfrey@users.noreply.github.com>
2025-02-26 12:05:14 +0100

    Write basic upgrade script

--- tools/upgrade.sh
+++ tools/upgrade.sh
@@ -5,27 +5,37 @@ set -e
 # Take first argument as the version to upgrade to
 VERSION=$1
 
-# TODO: make this conditional, just for docker
-SUDO="sudo"
+if [ -z "$VERSION" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+if 
+
+# conditional sudo, just for docker
+if groups | grep -q docker; then 
+  SUDO="";
+else
+  SUDO="sudo";
+fi
 
 # pull this version to ensure we have it
 if ! ${SUDO} docker pull ghcr.io/lay3rlabs/wavs:${VERSION}; then
-    echo "Failed to pull ghcr.io/lay3rlabs/wavs:${VERSION}"
+    echo "Invalid WAVS version, cannot pull ghcr.io/lay3rlabs/wavs:${VERSION}"
     exit 1
 fi
 
 # Update Makefile
-sed -i "s/ghcr.io\/lay3rlabs\/wavs:[^\s]+/ghcr.io\/lay3rlabs\/wavs:${VERSION}/g" Makefile
+sed -E -i "s/ghcr.io\/lay3rlabs\/wavs:[^ ]+/ghcr.io\/lay3rlabs\/wavs:${VERSION}/g" Makefile
 
 # Update docker-compose.yml
-sed -i "s/   image: \"ghcr.io\/lay3rlabs\/wavs:[^\"]+/   image: \"ghcr.io\/lay3rlabs\/wavs:${VERSION}/g" docker-compose.yml
+sed -E -i "s/ghcr.io\/lay3rlabs\/wavs:[^\"]+/ghcr.io\/lay3rlabs\/wavs:${VERSION}/g" docker-compose.yml
 
 # Update Cargo.toml (for crates dependencies)
-sed -i "s/wavs-wasi-chain = \"[^\"]+/wavs-wasi-chain = \"${VERSION}/g" Cargo.toml
+sed -E -i "s/wavs-wasi-chain = \"[^\"]+/wavs-wasi-chain = \"${VERSION}/g" Cargo.toml
 
 # Update [package.metadata.component] in components/*/Cargo.toml (for wit)
-sed -i "s/wavs:worker\/layer-trigger-world@[^\"]+/wavs:worker\/layer-trigger-world@${VERSION}/g" components/*/Cargo.toml
+sed -E -i "s/wavs:worker\/layer-trigger-world@[^\"]+/wavs:worker\/layer-trigger-world@${VERSION}/g" components/*/Cargo.toml
 
-# Rebuild with cargo component build
-
- 
+# Rebuild with cargo component build in order to update bindings and Cargo.lock
+rm components/*/src/bindings.rs
+make wasi-build
