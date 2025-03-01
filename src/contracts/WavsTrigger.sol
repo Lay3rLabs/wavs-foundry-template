a2f1a918bb287833e2cf867ae6a242296169d336
Reece Williams <reecepbcups@gmail.com>
2025-03-01 11:15:55 -0600

    move back to TriggerId type

--- src/contracts/WavsTrigger.sol
+++ src/contracts/WavsTrigger.sol
@@ -5,18 +5,18 @@ import {ISimpleTrigger} from "interfaces/IWavsTrigger.sol";
 
 contract SimpleTrigger is ISimpleTrigger {
     /// @inheritdoc ISimpleTrigger
-    uint64 public nextTriggerId = 0;
+    TriggerId public nextTriggerId;
 
     /// @inheritdoc ISimpleTrigger
-    mapping(uint64 _triggerId => Trigger _trigger) public triggersById;
+    mapping(TriggerId _triggerId => Trigger _trigger) public triggersById;
     /// @notice See ISimpleTrigger.triggerIdsByCreator
-    mapping(address _creator => uint64[] _triggerIds) internal _triggerIdsByCreator;
+    mapping(address _creator => TriggerId[] _triggerIds) internal _triggerIdsByCreator;
 
     /// @inheritdoc ISimpleTrigger
     function addTrigger(bytes memory _data) external {
         // Get the next trigger id
-        // nextTriggerId = TriggerId.wrap(TriggerId.unwrap(nextTriggerId) + 1);
-        uint64 _triggerId = nextTriggerId;
+        nextTriggerId = TriggerId.wrap(TriggerId.unwrap(nextTriggerId) + 1);
+        TriggerId _triggerId = nextTriggerId;
 
         // Create the trigger
         Trigger memory _trigger = Trigger({creator: msg.sender, data: _data});
@@ -32,13 +32,13 @@ contract SimpleTrigger is ISimpleTrigger {
     }
 
     /// @inheritdoc ISimpleTrigger
-    function getTrigger(uint64 triggerId) external view override returns (TriggerInfo memory _triggerInfo) {
+    function getTrigger(TriggerId triggerId) external view override returns (TriggerInfo memory _triggerInfo) {
         Trigger storage _trigger = triggersById[triggerId];
         _triggerInfo = TriggerInfo({triggerId: triggerId, creator: _trigger.creator, data: _trigger.data});
     }
 
     /// @inheritdoc ISimpleTrigger
-    function triggerIdsByCreator(address _creator) external view returns (uint64[] memory _triggerIds) {
+    function triggerIdsByCreator(address _creator) external view returns (TriggerId[] memory _triggerIds) {
         _triggerIds = _triggerIdsByCreator[_creator];
     }
 }
--- src/interfaces/IWavsSubmit.sol
+++ src/interfaces/IWavsSubmit.sol
@@ -12,19 +12,19 @@ interface ISimpleSubmit is ITypes {
      * @param _triggerId The identifier of the trigger
      * @return _isValid True if the trigger is valid, false otherwise
      */
-    function isValidTriggerId(uint64 _triggerId) external view returns (bool _isValid);
+    function isValidTriggerId(TriggerId _triggerId) external view returns (bool _isValid);
 
     /**
      * @notice Get the signature for a triggerId
      * @param _triggerId The identifier of the trigger
      * @return _signature The signature associated with the trigger
      */
-    function getSignature(uint64 _triggerId) external view returns (bytes memory _signature);
+    function getSignature(TriggerId _triggerId) external view returns (bytes memory _signature);
 
     /**
      * @notice Get the data for a triggerId
      * @param _triggerId The identifier of the trigger
      * @return _data The data associated with the trigger
      */
-    function getData(uint64 _triggerId) external view returns (bytes memory _data);
+    function getData(TriggerId _triggerId) external view returns (bytes memory _data);
 }
