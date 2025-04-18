9a178dd8a520245382f881437a2eef600b162336
Reece Williams <reecepbcups@gmail.com>
2025-04-18 12:00:12 -0500

    add bash Cmd runner

--- src/common/CmdRunner.sol
+++ src/common/CmdRunner.sol
@@ -5,14 +5,14 @@ pragma solidity 0.8.22;
 
 import "forge-std/Script.sol";
 
-contract CmdRunner is Script {
+contract CmdRunner is Script{
     // `ffi = true` must be set in foundry.toml
     //
     // Run a command and return the output by creating a temporary script with
     // the entire command and running it via bash. This gets around the limits
     // of FFI, such as not being able to pipe between two commands.
     // string memory entry = runCmd(string.concat("curl -s ", url, " | jq -c .tree[0]"));
-    function runCmd(string memory cmd) internal returns (string memory) {
+    function runCmd(string memory cmd) external returns (string memory) {
         string memory script = string.concat(vm.projectRoot(), "/.ffirun.sh");
         // Save the cmd to a file
         vm.writeFile(script, cmd);
