8bf7884a5129a878b5afb8f3ce8e37d9cdd723d8
ismellike <glopez11193@gmail.com>
2026-02-27 16:54:25 -0600

    Set wavs tag 216aa67

--- deploy/constants.ts
+++ deploy/constants.ts
@@ -6,7 +6,7 @@ export const DEPLOYMENT_SUMMARY_FILE = ".docker/deployment_summary.json";
 /**
  * The WAVS Docker image.
  */
-export const WAVS_DOCKER_IMAGE = "ghcr.io/lay3rlabs/wavs:local";
+export const WAVS_DOCKER_IMAGE = "ghcr.io/lay3rlabs/wavs:216aa67";
 
 /**
  * The POA Middleware Docker image.
